3e857a50fbef1be9f47c6f42dbe73f634771c240
Cristian-developer <cdpacheco@uniguajira.edu.co>
2020-04-05 17:28:12 -0500

    Modificando el index

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    
+<p>Este es un archivo modificado por Cristian Pacheco</p>
 </body>
 </html>
